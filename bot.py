6b6899b23c478e7dfb638f13a57cf50a45ed8ff3
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-07-31 18:32:42 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,6 +0,0 @@
-
-#devansh please call API in this file and get_Chat_response should be returning response
-
-def get_Chat_response(Query):
-    return "Bank of baroda API call response from Azure"
-
